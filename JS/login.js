669860a0e93b6e59f7a995f341e2722218f45dee
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-06 14:54:18 -0500

    Nothing big changed, just committing changes

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,7 @@
+auth2 = gapi.auth2.init({
+    client_id: '245083786179-vrv7rk4j6haaknbuut85u0prmenijisi.apps.googleusercontent.com',
+    cookiepolicy: 'single_host_origin', /** Default value **/
+    scope: "profile  https://www.googleapis.com/auth/drive.file  https://www.googleapis.com/auth/plus.me" });
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
@@ -13,8 +17,39 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
+    document.getElementById("name").innerHTML = "ID: ";
+    document.getElementById("img").src = "";
+    document.getElementById("email").innerHTML = "Email: ";
+
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
+}
+
+function ensureUploadFolderPresent(){
+    return gapi.client.drive.files.list(
+        {q:"mimeType = 'application/vnd.google-apps.folder' and trashed = false"}
+    ).then(function(files){
+        var directory=files.result.items;
+
+        if(!directory.length){
+            return createFolder().then(function(res){
+                return res.result;
+            });
+        }else{
+            return directory[0];
+        }
+    });
+}
+
+function createFolder(){
+    return gapi.client.drive.files.insert(
+        {
+            'resource':{
+                "title":'Drive API From JS Sample',
+                "mimeType": "application/vnd.google-apps.folder"
+            }
+        }
+    )
 }
