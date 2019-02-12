782f8f5bb8560c9194b326082565e1175e95e89f
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-12 17:09:00 -0500

    Change codebase to follow google Drive API Quickstart file

--- JS/login.js
+++ JS/login.js
@@ -1,55 +1,105 @@
-auth2 = gapi.auth2.init({
-    client_id: '245083786179-vrv7rk4j6haaknbuut85u0prmenijisi.apps.googleusercontent.com',
-    cookiepolicy: 'single_host_origin', /** Default value **/
-    scope: "profile  https://www.googleapis.com/auth/drive.file  https://www.googleapis.com/auth/plus.me" });
+      // Client ID and API key from the Developer Console
+      var CLIENT_ID = '245083786179-dbrksanqevsqfsbjpfmt7qg51gma7duu.apps.googleusercontent.com';
+      var API_KEY = 'AIzaSyCxGYSUrYK4sICL_jLxFMpvFbhaZLg3Lxw';
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
-    //document.getElementById("id").innerHTML = "ID: " + profile.getID();
-    document.getElementById("name").innerHTML = "ID: " + profile.getName();
-    document.getElementById("img").src = profile.getImageUrl();
-    document.getElementById("email").innerHTML = "Email: " + profile.getEmail();
-}
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = 'profile https://www.googleapis.com/auth/drive.appdata';
 
-function signOut() {
-    document.getElementById("name").innerHTML = "ID: ";
-    document.getElementById("img").src = "";
-    document.getElementById("email").innerHTML = "Email: ";
+      var authorizeButton = document.getElementById('authorize_button');
+      var signoutButton = document.getElementById('signout_button');
 
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
+      /**
+       *  On load, called to load the auth2 library and API client library.
+       */
+      function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
 
-function ensureUploadFolderPresent(){
-    return gapi.client.drive.files.list(
-        {q:"mimeType = 'application/vnd.google-apps.folder' and trashed = false"}
-    ).then(function(files){
-        var directory=files.result.items;
+      /**
+       *  Initializes the API client library and sets up sign-in state
+       *  listeners.
+       */
+      function initClient() {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-        if(!directory.length){
-            return createFolder().then(function(res){
-                return res.result;
-            });
-        }else{
-            return directory[0];
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          authorizeButton.onclick = handleAuthClick;
+          signoutButton.onclick = handleSignoutClick;
+        }, function(error) {
+          appendPre(JSON.stringify(error, null, 2));
+        });
+      }
+
+      /**
+       *  Called when the signed in status changes, to update the UI
+       *  appropriately. After a sign-in, the API is called.
+       */
+      function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+          listFiles();
+        } else {
+          authorizeButton.style.display = 'block';
+          signoutButton.style.display = 'none';
         }
-    });
-}
+      }
 
-function createFolder(){
-    return gapi.client.drive.files.insert(
-        {
-            'resource':{
-                "title":'Drive API From JS Sample',
-                "mimeType": "application/vnd.google-apps.folder"
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node. Used to display the results of the API call.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+        var pre = document.getElementById('content');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+
+      /**
+       * Print files.
+       */
+      function listFiles() {
+        gapi.client.drive.files.list({
+          'pageSize': 10,
+          'fields': "nextPageToken, files(id, name)"
+        }).then(function(response) {
+          appendPre('Files:');
+          var files = response.result.files;
+          if (files && files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+              var file = files[i];
+              appendPre(file.name + ' (' + file.id + ')');
             }
-        }
-    )
-}
+          } else {
+            appendPre('No files found.');
+          }
+        });
+      }
